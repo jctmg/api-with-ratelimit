ce8d4536a4f2d84c61976a22747b01c0b7f56b43
japzio <jasper.developer@gmail.com>
2024-12-05 16:18:18 +0800

    initial commit

---
+++ src/main/java/com/tmg/api_with_ratelimit/ApiWithRatelimitApplication.java
@@ -0,0 +1,13 @@
+package com.tmg.api_with_ratelimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithRatelimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWithRatelimitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmg/api_with_ratelimit/ApiWithRatelimitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmg.api_with_ratelimit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWithRatelimitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
